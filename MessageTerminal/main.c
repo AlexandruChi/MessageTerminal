6b96911764a5d8fc2bf2861ec130ee45045b3785
Alexandru Chiriac <59575686+AlexandruChi@users.noreply.github.com>
2022-07-05 14:20:19 +0300

    Initial Commit

---
+++ MessageTerminal/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MessageTerminal
+//
+//  Created by Аʌєѯаɴдрѹ Кѵріак on 05.07.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
